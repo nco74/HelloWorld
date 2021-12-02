60c21c22b5c34e68050c63c766144074be02432b
Noah Cohen <noah1974@gmail.com>
2021-12-02 14:30:31 +0200

    First Init

---
+++ VSprojects/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
